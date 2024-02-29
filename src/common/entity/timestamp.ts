01f1c403dbd58261a54e134bb960ed892f81c1d0
Hojip-Kim <rlaghwlq12@naver.com>
2024-02-29 14:53:05 +0900

    feat: TypeORM TimeStamp 공용 Entity 모듈 생성

---
+++ src/common/entity/timestamp.ts
@@ -0,0 +1,14 @@
+import {
+    CreateDateColumn,
+    UpdateDateColumn
+} from 'typeorm'
+
+export abstract class TimeStamp {
+
+    @CreateDateColumn({ type: 'timestamp', name: 'created_at' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ type: 'timestamp', name: 'updated_at' })
+    updatedAt: Date;
+
+}
