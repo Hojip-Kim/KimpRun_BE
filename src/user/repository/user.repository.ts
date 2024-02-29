ca0e48aa5797ab98c3532ab5f694cb09e8d91aad
Hojip-Kim <rlaghwlq12@naver.com>
2024-02-29 15:13:22 +0900

    feat: TypeORM UserRepository 생성

---
+++ src/user/repository/user.repository.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm'
+import { InjectRepository } from '@nestjs/typeorm'
+import { UserEntity } from '../user.entity';
+
+@Injectable()
+export class UserRepository extends Repository<UserEntity> {
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly repository: Repository<UserEntity>
+    ){
+        super(repository.target, repository.manager, repository.queryRunner)
+    }
+}
