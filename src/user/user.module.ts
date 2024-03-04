af85aae5eb110f57602c3a9c3afadd93a55067bc
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-04 14:08:59 +0900

    refactor: userModule 의존성주입

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserRepository } from './repository/user.repository';
+import { User } from './user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GoogleStrategy } from 'src/auth/auth/strategy/GoogleStrategy';
+import { AuthModule } from 'src/auth/auth/auth.module';
+import { ConfigService } from '@nestjs/config'
 
 @Module({
-    providers: [UserService],
-    exports: [UserService]
+    imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
+    providers: [UserService, UserRepository, GoogleStrategy, ConfigService],
+    exports: [UserService, UserRepository]
 })
 export class UserModule {}
