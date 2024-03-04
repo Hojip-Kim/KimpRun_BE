7b6e3ed0176180566c2413670988600366c6fc3b
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-04 14:03:55 +0900

    feat: googleStrategy 구현 refactor: configService 적용

--- src/auth/auth/strategy/GoogleStrategy.ts
+++ src/auth/auth/strategy/GoogleStrategy.ts
@@ -1,16 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { INestApplication, Injectable } from '@nestjs/common';
 import { PassportStrategy} from '@nestjs/passport'
 import { Profile, Strategy } from 'passport-google-oauth20'
+import { BasicOauthUser } from 'src/user/interface/googleUser';
 import { User } from 'src/user/user.entity';
 import { UserService } from 'src/user/user.service';
+import { ConfigService } from '@nestjs/config'
+
+export interface extendGoogle extends BasicOauthUser {
+    provider : string
+}
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy) {
-    constructor(private userService: UserService){
-
+    constructor(private userService: UserService, private configService : ConfigService){
         super({
-            clientID: process.env.GOOGLE_CLIENT_ID,
-            client_secret: process.env.GOOGLE_SECURITY_SECRET,
+            clientID: configService.get('GOOGLE_CLIENT_ID'),
+            client_secret: configService.get('GOOGLE_SECURITY_SECRET'),
             callbackURL: 'http://localhost:3000/api/auth/google',
             scope: ['email', 'profile'],
         });
@@ -21,13 +26,16 @@ export class GoogleStrategy extends PassportStrategy(Strategy) {
         const { id, name, emails } = profile;
 
         const providerId = id;
-        const email = emails[0].value;
 
-        const user: User = await this.userService.findByEmailOrSave(
-            email,
-            name.familyName + name.givenName,
-            providerId
+        const googleUser : extendGoogle = {
+            email : emails[0].value,
+            name : name.familyName + name.givenName,
+            providerId : providerId,
+            provider : 'Google'
+        }
+        const user: User = await this.userService.findByEmailOrSave(googleUser
         )
+
         return user;
     }
 }
