9738f3216d370490a873a4dd1026f0b02bc907e1
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-10 21:12:40 +0900

    feat: EventsController 거래소별 stream controller 생성

--- src/sse_events/events.controller.ts
+++ src/sse_events/events.controller.ts
@@ -3,17 +3,28 @@ import { EventService } from './eventService';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { sseInterface } from './interface/sseInterface';
+import { BinanceEventService } from './binanceEventService';
+import { UpbitEventService } from './upbitEventService';
 
 @Controller('events')
 export class EventsController {
 
-    constructor(private eventService : EventService){}
+    constructor(private binanceEventService : BinanceEventService,
+        private upbitEventService : UpbitEventService){}
 
-    @Sse('stream')
-    streamEvents(): Observable<sseInterface> {
+    @Sse('binance')
+    async binanceEvents(): Promise<Observable<sseInterface>> {
 
-        return this.eventService.getData().pipe(
+        return await this.binanceEventService.getBinanceData().pipe(
             map(data => ({ data: JSON.stringify(data) }))
+        );
+    }
+
+    @Sse('upbit')
+    async upbitEvents(): Promise<Observable<sseInterface>>{
+
+        return await this.upbitEventService.getUpbitData().pipe(
+            map(data => ({ data: JSON.stringify(data)}))
         );
     }
 
