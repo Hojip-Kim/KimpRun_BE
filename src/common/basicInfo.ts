dd9f2bbe9c50e25307cbb8b14189b1d55556667c
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-12 11:32:46 +0900

    feat: basicInfo 전역변수 설정 common service 생성

---
+++ src/common/basicInfo.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class basicInfo {
+
+    public static tether : number;
+
+    public static dominance : number;
+
+    public static bitcoinAmount : number;
+
+}
