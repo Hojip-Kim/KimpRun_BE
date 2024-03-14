6aaf4a4eab672643aa12b8d35ac9d5ff9b04243c
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-14 15:13:55 +0900

    feat: .env 개발/프로덕션 환경설정 및 providers 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,39 +6,51 @@ import { AuthModule } from './auth/auth/auth.module';
 import { EventsController } from './sse_events/events.controller';
 import { BinanceService } from './websocket/BinanceService';
 import { UpbitService } from './websocket/upbitService';
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User } from './user/user.entity';
 import { Profile } from './profile/entity/profile';
 import { BoardsEntity } from './community/boards/boards/boards.entity';
 import { BinanceEventService } from './sse_events/binanceEventService';
 import { UpbitEventService } from './sse_events/upbitEventService';
-import { ScheduleModule } from '@nestjs/schedule'
+import { ScheduleModule } from '@nestjs/schedule';
 import { ExportTradePair } from './websocket/tradepair/tradepair';
+import { CronInfo } from './common/cronInfo';
 
 @Module({
   imports: [
-    UserModule, 
+    UserModule,
     AuthModule,
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
     ScheduleModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService : ConfigService) => ({
-      type: 'postgres',
-      host: configService.get<string>('TYPEORM_HOST'),
-      port: configService.get<number>('TYPEORM_PORT'),
-      entities: [User, BoardsEntity, Profile],
-      username: configService.get<string>('TYPEORM_USERNAME'),
-      password: configService.get<string>('TYPEORM_PASSWORD'),
-      database: configService.get<string>('TYPEORM_DATABASE'),
-      autoLoadEntities: true,
-      synchronize: true,
-      })
-    })
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('TYPEORM_HOST'),
+        port: configService.get<number>('TYPEORM_PORT'),
+        entities: [User, BoardsEntity, Profile],
+        username: configService.get<string>('TYPEORM_USERNAME'),
+        password: configService.get<string>('TYPEORM_PASSWORD'),
+        database: configService.get<string>('TYPEORM_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
   ],
   controllers: [AppController, EventsController],
-  providers: [AppService, BinanceEventService, UpbitEventService, BinanceService, UpbitService, ExportTradePair],
+  providers: [
+    AppService,
+    BinanceEventService,
+    UpbitEventService,
+    BinanceService,
+    UpbitService,
+    ExportTradePair,
+    CronInfo,
+  ],
 })
 export class AppModule {}
